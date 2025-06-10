d6fe6d1933795fc2b31e3014cd216c9ceaf55596
Franco <cbabot350@gmail.com>
2025-06-10 18:23:54 -0400

    host agregado de nuevo

--- SCMAA/settings.py
+++ SCMAA/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = 'django-insecure-c@i7n+gr)68k&@374a^-(*q_15imo35e7-(i*=+gp^yn+gojjv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'https://scmaa.onrender.com',
-]
+ALLOWED_HOSTS = ['scmaa.onrender.com']
 
 
 # Application definition
