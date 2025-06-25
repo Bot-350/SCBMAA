a6177a59468c9411ba019e8ec74e6580d8be83c4
Franco <cbabot350@gmail.com>
2025-06-24 20:39:59 -0400

    static arreglado

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    # ...existing code...
-]
-
-# Agrega las rutas estáticas directamente a urlpatterns
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
